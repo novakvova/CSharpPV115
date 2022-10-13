e7d4c429b4da2fa2212dd0011a62c0534ec32877
novakvova <novakvova@gmail>
2022-10-13 19:25:26 +0300

    Ovveride method

---
+++ _9_Extention_Net/MyPoint.cs
@@ -0,0 +1,61 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Extention_Net
+{
+    public class MyPoint : IComparable<MyPoint>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public MyPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString() => $"[{this.X}, {Y}]";
+
+        public static MyPoint operator+(MyPoint p1, MyPoint p2)
+        {
+            return new MyPoint(p1.X+p2.X, p1.Y+p2.Y);
+        }
+        public static MyPoint operator-(MyPoint p1, MyPoint p2)
+        {
+            return new MyPoint(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static MyPoint operator++(MyPoint p1)
+        {
+            return new MyPoint(p1.X+1, p1.Y+1);
+        }
+
+        public override bool Equals(object? obj) => obj.ToString() == this.ToString();
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public int CompareTo(MyPoint? other)
+        {
+            if (this.X > other.X && this.Y > other.Y)
+                return 1;
+            if (this.X < other.X && this.Y < other.Y)
+                return -1;
+            else
+                return 0;
+        }
+
+        public static bool operator <(MyPoint p1, MyPoint p2) => p1.CompareTo(p2) < 0;
+        public static bool operator >(MyPoint p1, MyPoint p2) => p1.CompareTo(p2) > 0;
+        public static bool operator >=(MyPoint p1, MyPoint p2) => p1.CompareTo(p2) >= 0;
+        public static bool operator <=(MyPoint p1, MyPoint p2) => p1.CompareTo(p2) <= 0;
+
+        public static bool operator==(MyPoint p1, MyPoint p2) => p1.Equals(p2);
+        public static bool operator!=(MyPoint p1, MyPoint p2) => !p1.Equals(p2);
+
+    }
+}
---
+++ _9_Extention_Net/Program.cs
@@ -0,0 +1,54 @@
+﻿using _9_Extention_Net;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Drawing;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace _Extention_Net
+{
+    static class Program
+    {
+        public static void PointInfo(this MyPoint p)
+        {
+            Console.WriteLine("Point = {0}", p);
+        }
+        public static void Main(string[] args)
+        {
+            //DataTable dt = new DataTable();
+            //dt.TableName = "Список користувачів";
+            //dt.Columns.Add("Електронна пошта", typeof(string));
+            //dt.Columns.Add("ПІБ", typeof(string));
+            //dt.Rows.Add("ivan@gmail.com", "Іван Васильно");
+            //dt.Rows.Add("peter@gmail.com", "Петро Юхим");
+            //dt.Rows.Add("lena@gmail.com", "Олена Трудовик");
+            //dt.AcceptChanges();
+            //string fileName = "mydata.xlsx";
+            //using(XLWorkbook wb = new XLWorkbook())
+            //{
+            //    wb.Worksheets.Add(dt);
+            //    using(FileStream fs = new FileStream(fileName, FileMode.Create))
+            //    {
+            //        wb.SaveAs(fs);
+            //    }
+            //}
+
+            MyPoint a = new MyPoint(2,4);
+            MyPoint b = new MyPoint(1,2);
+            MyPoint c = a + b;
+
+            Console.WriteLine("c = {0}", c);
+            c++;
+            Console.WriteLine("c++ = {0}", c);
+            Console.WriteLine("a>b = {0}", a>b);
+            Console.WriteLine("a<b = {0}", a<b);
+
+            Console.WriteLine("a==b = {0}", a==b);
+            Console.WriteLine("a!=b = {0}", a!=b);
+
+            a.PointInfo();
+            string str = "Марина18";
+            Console.WriteLine(str.Count());
+        }
+    }
+}
+
