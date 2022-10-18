6ea433f4c67c386a7e4926c8789819c498adec36
novakvova <novakvova@gmail>
2022-10-18 19:29:31 +0300

    Garbage Collector

---
+++ _11_GB/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_GB
+{
+    public class Car :IDisposable
+    {
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        public Car()
+        {
+
+        }
+        public Car(string name, int speed)
+        {
+            PetName = name;
+            CurrentSpeed = speed;
+        }
+        public override string ToString()
+        {
+            return $"{PetName} is going {CurrentSpeed} MPH";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Обєект буде видалено");
+        }
+    }
+}
---
+++ _11_GB/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _11_GB
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("_____GC Basic_____");
+            Console.WriteLine("Кільлькість байт виділених для кучі: {0}",
+                GC.GetTotalMemory(false));
+            Console.WriteLine("Кількість згенерованих обєктів в ОС: {0}",
+               GC.MaxGeneration);
+            string str = "Hello";
+            using (Car audi = new Car("Audi A4", 50))
+            {
+                Console.WriteLine(audi);
+                Console.WriteLine("Create new object");
+                Console.WriteLine("Покоління обєкта в кучі audi: {0}",
+                    GC.GetGeneration(audi));
+            }
+            GC.Collect();
+
+            Car reno = new Car("Honda Civic", 50);         
+
+            Console.WriteLine("Покоління обєкта в кучі reno: {0}",
+                GC.GetGeneration(reno));
+
+            Console.WriteLine("Адреса памяті audi: {0}", Get(reno));
+
+            Console.WriteLine("Кільлькість байт виділених для кучі: {0}", 
+                GC.GetTotalMemory(false));
+        }
+
+        static void MakeCar()
+        {
+            Car car = new Car();
+            car = null;
+        }
+
+        public static string Get(object a)
+        {
+            ref object r = ref a;
+            //roughly equivalent to
+            TypedReference tr = __makeref(a);
+            return tr.GetHashCode().ToString();
+            //Console.WriteLine("tr {0}", tr.GetHashCode());
+            //GCHandle objHandle = GCHandle.Alloc(a, GCHandleType.WeakTrackResurrection);
+            
+            //IntPtr pObj = objHandle.AddrOfPinnedObject();
+            //return pObj.ToString();
+        }
+    }
+}
+
